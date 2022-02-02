cebea304e56ba7785cb653caab1fd794da36742f
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2022-02-02 18:42:02 -0300

    Adição dos scripts e da collection

---
+++ Scripts.sql
@@ -0,0 +1,23 @@
+CREATE TABLE [dbo].[Usuarios] (
+    [Id]                UNIQUEIDENTIFIER NOT NULL,
+    [Nome]              VARCHAR (200)    NOT NULL,
+    [Cpf]               VARCHAR (200)    NOT NULL,
+    [DataNascimento]    DATETIME2        NOT NULL,
+    [RendaMensal]       DECIMAL (18)     NOT NULL,
+    [PlanoVip]          BIT              DEFAULT ((0)) NOT NULL,
+    [DataCadastramento] DATETIME2        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[EnderecoUsuario] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Logradouro]  VARCHAR (200)    NOT NULL,
+    [Bairro]      VARCHAR (50)     NOT NULL,
+    [Cep]         VARCHAR (8)      NOT NULL,
+    [Cidade]      VARCHAR (50)     NOT NULL,
+    [UF]          VARCHAR (50)     NOT NULL,
+    [Complemento] VARCHAR (50)     NULL,
+    [UsuarioId]   UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UsuarioId]) REFERENCES [dbo].[Usuarios] ([Id]) ON DELETE CASCADE
+);
